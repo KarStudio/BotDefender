215048d0c2e62830352fe31ad4fc88d9857fbdfe
Ghost_chu <2908803755@qq.com>
2022-02-01 19:08:06 +0800

    新增 RPC 支持

--- src/main/java/com/ghostchu/botdefender/blocker/BlockController.java
+++ src/main/java/com/ghostchu/botdefender/blocker/BlockController.java
@@ -1,22 +1,11 @@
 package com.ghostchu.botdefender.blocker;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.net.InetAddress;
 import java.time.Duration;
-import java.util.List;
-import java.util.Map;
 
 public interface BlockController {
-    /**
-     * 检查指定的 IP 地址是否处于封锁列表中
-     * @param address IP 地址
-     * @return 封禁剩余时长，如果不存在，返回 null
-     */
-    @Nullable
-    Duration isBlocked(@NotNull InetAddress address);
-
     /**
      * 封锁指定的 IP 地址
      * @param address IP 地址
@@ -29,11 +18,4 @@ public interface BlockController {
      * @param address IP 地址
      */
     void unblock(@NotNull InetAddress address);
-
-    /**
-     * 获取所有封锁列表
-     * @return 封锁列表 [IP,剩余时长]
-     */
-    @NotNull
-    List<Map.Entry<InetAddress, Duration>> getBlockList();
 }
---
+++ src/main/java/com/ghostchu/botdefender/rpc/client/BlockControllerRPC.java
@@ -0,0 +1,75 @@
+package com.ghostchu.botdefender.rpc.client;
+
+import com.ghostchu.botdefender.BotDefender;
+import com.ghostchu.botdefender.blocker.BlockController;
+import com.ghostchu.botdefender.rpc.proto.BlockControllerGrpc;
+import com.ghostchu.botdefender.rpc.proto.BlockControllerProto;
+import com.ghostchu.simplereloadlib.ReloadResult;
+import com.ghostchu.simplereloadlib.Reloadable;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.jetbrains.annotations.NotNull;
+
+import java.net.InetAddress;
+import java.time.Duration;
+
+public class BlockControllerRPC implements Reloadable, BlockController {
+    private final BotDefender plugin;
+    private String user;
+    private String target;
+    private ManagedChannel channel;
+    private BlockControllerGrpc.BlockControllerBlockingStub blockingStub;
+    public BlockControllerRPC(@NotNull BotDefender plugin) {
+        this.plugin = plugin;
+        plugin.getReloadManager().register(this);
+        init();
+    }
+    public void init(){
+        this.user = plugin.getConfig().getString("rpc.user");
+        this.target = plugin.getConfig().getString("rpc.target");
+        if(this.channel != null)
+            this.channel.shutdownNow();
+        this.channel = ManagedChannelBuilder.forTarget(target)
+                // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
+                // needing certificates.
+                .usePlaintext()
+                .build();
+        blockingStub = BlockControllerGrpc.newBlockingStub(channel);
+    }
+
+
+    @Override
+    public ReloadResult reloadModule() throws Exception {
+        init();
+        return Reloadable.super.reloadModule();
+    }
+
+    /**
+     * 封锁指定的 IP 地址
+     *
+     * @param address  IP 地址
+     * @param duration 持续时间
+     */
+    @Override
+    public void block(@NotNull InetAddress address, @NotNull Duration duration) {
+        BlockControllerProto.Address rpcAddress = BlockControllerProto.Address.newBuilder().setAddress(address.getHostAddress()).build();
+        BlockControllerProto.BlockRequest rpcBlockRequest = BlockControllerProto.BlockRequest.newBuilder()
+                .setAddress(rpcAddress)
+                .setEndTime(System.currentTimeMillis()+duration.toMillis())
+                .build();
+        //noinspection ResultOfMethodCallIgnored
+        blockingStub.blockAddress(rpcBlockRequest);
+    }
+
+    /**
+     * 解除指定 IP 地址的封锁
+     *
+     * @param address IP 地址
+     */
+    @Override
+    public void unblock(@NotNull InetAddress address) {
+        BlockControllerProto.Address rpcAddress = BlockControllerProto.Address.newBuilder().setAddress(address.getHostAddress()).build();
+        //noinspection ResultOfMethodCallIgnored
+        blockingStub.unblockAddress(rpcAddress);
+    }
+}
